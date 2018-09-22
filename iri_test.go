12d2d1e70bf2fced53d8f41bab1932587cf8c87f
garsue <osuga.shunya@mfkessai.co.jp>
2018-09-22 16:33:41 +0900

    Add iri_test.go

---
+++ iri_test.go
@@ -0,0 +1,77 @@
+package sparql
+
+import "testing"
+
+func TestIRI_Ref(t *testing.T) {
+	tests := []struct {
+		name string
+		i    IRI
+		want string
+	}{
+		{
+			name: "replace <",
+			i:    "<",
+			want: "<%3C>",
+		},
+		{
+			name: "replace >",
+			i:    ">",
+			want: "<%3E>",
+		},
+		{
+			name: `replace "`,
+			i:    `"`,
+			want: "<%22>",
+		},
+		{
+			name: "replace space",
+			i:    " ",
+			want: "<%20>",
+		},
+		{
+			name: "replace {",
+			i:    "{",
+			want: "<%7B>",
+		},
+		{
+			name: "replace }",
+			i:    "}",
+			want: "<%7D>",
+		},
+		{
+			name: "replace |",
+			i:    "|",
+			want: "<%7C>",
+		},
+		{
+			name: `replace \`,
+			i:    `\`,
+			want: "<%5C>",
+		},
+		{
+			name: "replace ^",
+			i:    "^",
+			want: "<%5E>",
+		},
+		{
+			name: "replace “",
+			i:    "`",
+			want: "<%60>",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.i.Ref(); got != tt.want {
+				t.Errorf("IRI.Ref() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestPrefixedName_Ref(t *testing.T) {
+	p := PrefixedName("foo")
+	want := "foo"
+	if got := p.Ref(); got != want {
+		t.Errorf("PrefixedName.Ref() = %v, want %v", got, want)
+	}
+}
